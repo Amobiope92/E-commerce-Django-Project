bbaa1767750f0e987305e06f4ea0b87e176f18d9
Amobi Opeyemi <amobiope92@gmail.com>
2024-04-24 12:31:55 +0100

    second commit

--- classswork/settings.py
+++ classswork/settings.py
@@ -79,9 +79,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':  'zay_classwork_project',
         'USER': 'root',
-        'PASSWORD': 'wonderkid92',
+        'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '',
         'OPTION': {
             'autocommit': True,
         }
@@ -140,5 +140,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_USE_TLS= True
-EMAIL_HOST_USER='wkid049@gmail.com'
-EMAIL_HOST_PASSWORD ='Wonderkid@92'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD =''
